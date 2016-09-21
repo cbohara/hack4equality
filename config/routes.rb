0846c48c7aba18d9489d332f9b2f1ea68f03d6dc
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-09-21 11:17:36 -0700

    successfully displaying /api/v1/companies.json!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :companies, only: [:index, :create, :update, :destroy]
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
