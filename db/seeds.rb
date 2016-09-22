656f19a7038897c8f985d50fd7e4676bd94c4af3
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-09-21 20:00:16 -0700

    working with modules is functional but want to build a class

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'httparty'
+
+module Glassdoor
+  include HTTParty
+  base_uri "api.glassdoor.com"
+
+  def self.company_name(company_name)
+    data = get("/api/api.htm?t.p=92900&t.k=jw0i2Cakuei&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&ps=1&q=#{company_name}").parsed_response
+    data['response']['employers'][0]['name']
+  end
+end
+
+p Glassdoor.company_name("trader joe's")
